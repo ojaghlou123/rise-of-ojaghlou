25043bbe0af77c1c5464facaa6fe4d7fa79a3b00
ojaghlou123 <ojaghloughoochali@gmail.com>
2025-10-04 11:34:51 +0330

    Simple changes.

--- ROO.py
+++ ROO.py
@@ -1,26 +1,26 @@
 import turtle
 import time
 
-# Setup screen
+# Screen setup
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("Simple Shooting Fire Animation")
+screen.tracer(0)
 
-# Create turtle for shooting fire
+# Create a fire projectile turtle
 fire = turtle.Turtle()
 fire.shape("circle")
 fire.color("orange")
 fire.penup()
 fire.speed(0)
 fire.goto(0, -200)
-fire.dy = 15  # speed of the fire moving upward
+fire.dy = 15  # vertical speed
 
 def shoot_fire():
     y = fire.ycor()
     y += fire.dy
     fire.sety(y)
 
-    # When fire reaches top of screen, reset to start position
     if y > 200:
         fire.hideturtle()
         fire.goto(0, -200)
@@ -30,6 +30,4 @@ def shoot_fire():
 while True:
     shoot_fire()
     screen.update()
-    time.sleep(0.03)  # control the speed
-
-screen.mainloop()
+    time.sleep(0.03)
